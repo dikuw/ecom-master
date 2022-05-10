30e68d182103a0810f4c8a5b8a1fab8fd9a77f1d
Michael Vasilevsky <michael@dikuw.com>
2022-05-10 18:25:11 -0400

    add handleChange

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -3,11 +3,22 @@ import { useState } from 'react';
 export default function CreateProduct() {
   const [name, setName] = useState('');
   
+  const handleChange = (e) => {
+    setName(e.target.value);
+  }
+
   return (
     <form>
       <label htmlFor="name">
         Name
-        <input type="text" id="name" name="name" placeholder="Name" />
+        <input 
+          type="text" 
+          id="name" 
+          name="name" 
+          placeholder="Name" 
+          value={name} 
+          onChange={handleChange} 
+        />
       </label>
     </form>
   )
