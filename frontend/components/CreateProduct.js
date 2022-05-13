c62c5895c7f7075f2f0171b5bb561bb39f40b269
Michael Vasilevsky <michael@dikuw.com>
2022-05-13 04:55:14 -0400

    add Form

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,4 +1,5 @@
 import useForm from '../lib/useForm';
+import Form from './styles/Form';
 
 export default function CreateProduct() {
   const { inputs, handleChange, resetForm, clearForm } = useForm({
@@ -8,7 +9,7 @@ export default function CreateProduct() {
   });
 
   return (
-    <form>
+    <Form>
       <label htmlFor="name">
         Name
         <input 
@@ -45,6 +46,6 @@ export default function CreateProduct() {
 
       <button type="button" onClick={resetForm}>Reset Form</button>
       <button type="button" onClick={clearForm}>Clear Form</button>
-    </form>
+    </Form>
   )
 }
