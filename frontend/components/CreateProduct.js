1ae4ea392b796e6526da32fddbb95a2dad51f49e
Michael Vasilevsky <michael@dikuw.com>
2022-05-13 05:01:31 -0400

    add image field

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -10,42 +10,51 @@ export default function CreateProduct() {
 
   return (
     <Form>
-      <label htmlFor="name">
-        Name
-        <input 
-          type="text" 
-          id="name" 
-          name="name" 
-          placeholder="Name" 
-          value={inputs.name} 
-          onChange={handleChange} 
-        />
-      </label>
-      <label htmlFor="price">
-        Price
-        <input 
-          type="number" 
-          id="price" 
-          name="price" 
-          placeholder="price" 
-          value={inputs.price} 
-          onChange={handleChange} 
-        />
-      </label>
-      <label htmlFor="description">
-        Description
-        <input 
-          type="text" 
-          id="description" 
-          name="description" 
-          placeholder="description" 
-          value={inputs.description} 
-          onChange={handleChange} 
-        />
-      </label>
-
-      <button type="submit">+ Add Product</button>
-
+      <fieldset>
+        <label htmlFor="image">
+          Image
+          <input 
+            type="file" 
+            id="image" 
+            name="image" 
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="name">
+          Name
+          <input 
+            type="text" 
+            id="name" 
+            name="name" 
+            placeholder="Name" 
+            value={inputs.name} 
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input 
+            type="number" 
+            id="price" 
+            name="price" 
+            placeholder="price" 
+            value={inputs.price} 
+            onChange={handleChange} 
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <input 
+            type="text" 
+            id="description" 
+            name="description" 
+            placeholder="description" 
+            value={inputs.description} 
+            onChange={handleChange} 
+          />
+        </label>
+        <button type="submit">+ Add Product</button>
+      </fieldset>
     </Form>
   )
 }
