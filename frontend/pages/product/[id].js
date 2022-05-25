0d0ace40d1a06ba831c172540b83349b7bde5de6
Michael Vasilevsky <michael@dikuw.com>
2022-05-25 17:13:27 -0400

    add query

--- frontend/pages/product/[id].js
+++ frontend/pages/product/[id].js
@@ -1,5 +1,23 @@
-export default function SingleProduct(){
+import gql from 'graphql-tag';
+import { useQuery } from "@apollo/client";
+
+const SINGLE_ITEM_QUERY = gql `
+  query {
+    Product(where:{
+      id: "6258948ac0401a13982109e0"
+    }) {
+      name
+      price
+      description
+    }
+  }
+`;
+
+export default function SingleProduct({ query }){
+  
+  const { data, loading, error } = useQuery(SINGLE_ITEM_QUERY);
+  
   return (
-    <p>single product</p>
+    <p>single product {query.id}</p>
   );
 }
